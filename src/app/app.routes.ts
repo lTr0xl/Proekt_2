77eb71d31f156e396b92e76c17e0248b59f6c195
Toshe Nikolov <nikolovtoshe22@gmail.com>
2024-02-23 09:55:15 +0100

    Proekt_2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { CoursePageComponent } from './course-page/course-page.component';
 import { CoursesHomeComponent } from './courses-home/courses-home.component';
 import { SurveyComponent } from './survey/survey.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
+import { SearchcompComponent } from './searchcomp/searchcomp.component';
 
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
-    {path:'search/:searchTerm', component:HomeComponent},
+    {path:'search/:searchTerm', component:SearchcompComponent},
     {path:'tag/:tag', component:CoursesHomeComponent},
     {path:'tag2/:tag2', component:HomeComponent},
     {path:'course/:id',component:CoursePageComponent},
---
+++ src/app/searchcomp/searchcomp.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchcompComponent } from './searchcomp.component';
+
+describe('SearchcompComponent', () => {
+  let component: SearchcompComponent;
+  let fixture: ComponentFixture<SearchcompComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SearchcompComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(SearchcompComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
